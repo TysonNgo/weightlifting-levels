769bd04c0fb4fac53616c5eaeecd7cde37d51c5c
tkn <tyson_ngo@hotmail.com>
2017-10-25 11:10:48 -0400

    implemented the calculator

---
+++ src/Levels.js
@@ -0,0 +1,863 @@
+function setResults(ref, label, value) {
+	function getWeightclass(){
+		var sex = ref.state.sex;
+		var bw = stats.bodyweight;
+		var weightclasses = levels[0][sex];
+		var result = weightclasses[0].bodyweight;
+		for (var i = 0; i < weightclasses.length-1; i++){
+			if (bw > weightclasses[i].bodyweight){
+				result = weightclasses[i+1].bodyweight;
+			} 
+		}
+		return result > 0 ? result : result*-1+"+";
+	}
+
+	function getResults(){
+		var result = {};
+		var sex = ref.state.sex;
+		result["weightclass"] = getWeightclass();
+		var wci = wcIndexes[sex+result["weightclass"]];
+
+		result.snatchLevel = 0;
+		result.cleanAndJerkLevel = 0;
+		result.backSquatLevel = 0;
+		result.frontSquatLevel = 0;
+
+		for (var i = 0; i < levels.length; i++){
+			var level = levels[i][sex][wci];
+			if (stats.snatch >= level.snatch)
+				result.snatchLevel = i+1;
+			if (stats.cleanAndJerk >= level.cleanAndJerk)
+				result.cleanAndJerkLevel = i+1;
+			if (stats.backSquat >= level.backSquat)
+				result.backSquatLevel = i+1;
+			if (stats.frontSquat >= level.frontSquat)
+				result.frontSquatLevel = i+1;
+		}
+
+		result.snatchNext = result.snatchLevel < 7?
+			levels[result.snatchLevel][sex][wci]["snatch"]: "";
+		result.cleanAndJerkNext = result.cleanAndJerkLevel < 7?
+			levels[result.cleanAndJerkLevel][sex][wci]["cleanAndJerk"]: "";
+		result.backSquatNext = result.backSquatLevel < 7?
+			levels[result.backSquatLevel][sex][wci]["backSquat"]: "";
+		result.frontSquatNext = result.frontSquatLevel < 7?
+			levels[result.frontSquatLevel][sex][wci]["frontSquat"]: "";
+
+		return result;
+	}
+
+	var wcIndexes = {
+		"men56": 0,
+		"men62": 1,
+		"men69": 2,
+		"men77": 3,
+		"men85": 4,
+		"men94": 5,
+		"men105": 6,
+		"men105+": 7,
+		"women48": 0,
+		"women53": 1,
+		"women58": 2,
+		"women63": 3,
+		"women69": 4,
+		"women75": 5,
+		"women75+": 6
+	};
+
+	var levels = [
+		{
+			men: [
+				{
+					bodyweight: 56,
+					snatch: 48,
+					cleanAndJerk: 59,
+					backSquat: 75,
+					frontSquat: 64
+				},
+				{
+					bodyweight: 62,
+					snatch: 53,
+					cleanAndJerk: 65,
+					backSquat: 83,
+					frontSquat: 71
+				},
+				{
+					bodyweight: 69,
+					snatch: 57,
+					cleanAndJerk: 70,
+					backSquat: 89,
+					frontSquat: 76
+				},
+				{
+					bodyweight: 77,
+					snatch: 61,
+					cleanAndJerk: 75,
+					backSquat: 95,
+					frontSquat: 82
+				},
+				{
+					bodyweight: 85,
+					snatch: 64,
+					cleanAndJerk: 79,
+					backSquat: 100,
+					frontSquat: 86
+				},
+				{
+					bodyweight: 94,
+					snatch: 67,
+					cleanAndJerk: 82,
+					backSquat: 105,
+					frontSquat: 90
+				},
+				{
+					bodyweight: 105,
+					snatch: 70,
+					cleanAndJerk: 86,
+					backSquat: 109,
+					frontSquat: 94
+				},
+				{
+					bodyweight: -105,
+					snatch: 75,
+					cleanAndJerk: 92,
+					backSquat: 117,
+					frontSquat: 100
+				}
+			],
+			women: [
+				{
+					bodyweight: 48,
+					snatch: 31,
+					cleanAndJerk: 38,
+					backSquat: 48,
+					frontSquat: 41
+				},
+				{
+					bodyweight: 53,
+					snatch: 35,
+					cleanAndJerk: 43,
+					backSquat: 55,
+					frontSquat: 47
+				},
+				{
+					bodyweight: 58,
+					snatch: 38,
+					cleanAndJerk: 47,
+					backSquat: 59,
+					frontSquat: 51
+				},
+				{
+					bodyweight: 63,
+					snatch: 40,
+					cleanAndJerk: 49,
+					backSquat: 62,
+					frontSquat: 54
+				},
+				{
+					bodyweight: 69,
+					snatch: 42,
+					cleanAndJerk: 52,
+					backSquat: 66,
+					frontSquat: 56
+				},
+				{
+					bodyweight: 75,
+					snatch: 45,
+					cleanAndJerk: 55,
+					backSquat: 70,
+					frontSquat: 60
+				},
+				{
+					bodyweight: -75,
+					snatch: 52,
+					cleanAndJerk: 64,
+					backSquat: 81,
+					frontSquat: 70
+				}
+			]
+		},
+		{
+			men: [
+				{
+					bodyweight: 56,
+					snatch: 64,
+					cleanAndJerk: 79,
+					backSquat: 100,
+					frontSquat: 86
+				},
+				{
+					bodyweight: 62,
+					snatch: 71,
+					cleanAndJerk: 87,
+					backSquat: 111,
+					frontSquat: 95
+				},
+				{
+					bodyweight: 69,
+					snatch: 75,
+					cleanAndJerk: 92,
+					backSquat: 117,
+					frontSquat: 100
+				},
+				{
+					bodyweight: 77,
+					snatch: 80,
+					cleanAndJerk: 98,
+					backSquat: 125,
+					frontSquat: 107
+				},
+				{
+					bodyweight: 85,
+					snatch: 85,
+					cleanAndJerk: 105,
+					backSquat: 133,
+					frontSquat: 114
+				},
+				{
+					bodyweight: 94,
+					snatch: 89,
+					cleanAndJerk: 109,
+					backSquat: 139,
+					frontSquat: 119
+				},
+				{
+					bodyweight: 105,
+					snatch: 92,
+					cleanAndJerk: 113,
+					backSquat: 144,
+					frontSquat: 123
+				},
+				{
+					bodyweight: -105,
+					snatch: 99,
+					cleanAndJerk: 122,
+					backSquat: 155,
+					frontSquat: 132
+				}
+			],
+			women: [
+				{
+					bodyweight: 48,
+					snatch: 41,
+					cleanAndJerk: 50,
+					backSquat: 64,
+					frontSquat: 55
+				},
+				{
+					bodyweight: 53,
+					snatch: 47,
+					cleanAndJerk: 58,
+					backSquat: 73,
+					frontSquat: 63
+				},
+				{
+					bodyweight: 58,
+					snatch: 50,
+					cleanAndJerk: 62,
+					backSquat: 78,
+					frontSquat: 67
+				},
+				{
+					bodyweight: 63,
+					snatch: 53,
+					cleanAndJerk: 65,
+					backSquat: 83,
+					frontSquat: 71
+				},
+				{
+					bodyweight: 69,
+					snatch: 57,
+					cleanAndJerk: 70,
+					backSquat: 89,
+					frontSquat: 76
+				},
+				{
+					bodyweight: 75,
+					snatch: 60,
+					cleanAndJerk: 74,
+					backSquat: 94,
+					frontSquat: 80
+				},
+				{
+					bodyweight: -75,
+					snatch: 69,
+					cleanAndJerk: 85,
+					backSquat: 108,
+					frontSquat: 92
+				}
+			]
+		},
+		{
+			men: [
+				{
+					bodyweight: 56,
+					snatch: 71,
+					cleanAndJerk: 86,
+					backSquat: 110,
+					frontSquat: 94
+				},
+				{
+					bodyweight: 62,
+					snatch: 78,
+					cleanAndJerk: 95,
+					backSquat: 121,
+					frontSquat: 104
+				},
+				{
+					bodyweight: 69,
+					snatch: 86,
+					cleanAndJerk: 105,
+					backSquat: 133,
+					frontSquat: 114
+				},
+				{
+					bodyweight: 77,
+					snatch: 94,
+					cleanAndJerk: 115,
+					backSquat: 146,
+					frontSquat: 125
+				},
+				{
+					bodyweight: 85,
+					snatch: 101,
+					cleanAndJerk: 124,
+					backSquat: 158,
+					frontSquat: 135
+				},
+				{
+					bodyweight: 94,
+					snatch: 105,
+					cleanAndJerk: 128,
+					backSquat: 163,
+					frontSquat: 140
+				},
+				{
+					bodyweight: 105,
+					snatch: 108,
+					cleanAndJerk: 133,
+					backSquat: 169,
+					frontSquat: 145
+				},
+				{
+					bodyweight: -105,
+					snatch: 117,
+					cleanAndJerk: 144,
+					backSquat: 183,
+					frontSquat: 157
+				}
+			],
+			women: [
+				{
+					bodyweight: 48,
+					snatch: 46,
+					cleanAndJerk: 57,
+					backSquat: 72,
+					frontSquat: 62
+				},
+				{
+					bodyweight: 53,
+					snatch: 52,
+					cleanAndJerk: 63,
+					backSquat: 81,
+					frontSquat: 69
+				},
+				{
+					bodyweight: 58,
+					snatch: 57,
+					cleanAndJerk: 70,
+					backSquat: 89,
+					frontSquat: 76
+				},
+				{
+					bodyweight: 63,
+					snatch: 61,
+					cleanAndJerk: 74,
+					backSquat: 95,
+					frontSquat: 81
+				},
+				{
+					bodyweight: 69,
+					snatch: 64,
+					cleanAndJerk: 78,
+					backSquat: 99,
+					frontSquat: 85
+				},
+				{
+					bodyweight: 75,
+					snatch: 66,
+					cleanAndJerk: 81,
+					backSquat: 103,
+					frontSquat: 88
+				},
+				{
+					bodyweight: -75,
+					snatch: 74,
+					cleanAndJerk: 90,
+					backSquat: 115,
+					frontSquat: 98
+				}
+			]
+		},
+		{
+			men: [
+				{
+					bodyweight: 56,
+					snatch: 77,
+					cleanAndJerk: 94,
+					backSquat: 119,
+					frontSquat: 102
+				},
+				{
+					bodyweight: 62,
+					snatch: 85,
+					cleanAndJerk: 104,
+					backSquat: 132,
+					frontSquat: 113
+				},
+				{
+					bodyweight: 69,
+					snatch: 96,
+					cleanAndJerk: 117,
+					backSquat: 149,
+					frontSquat: 128
+				},
+				{
+					bodyweight: 77,
+					snatch: 108,
+					cleanAndJerk: 132,
+					backSquat: 168,
+					frontSquat: 144
+				},
+				{
+					bodyweight: 85,
+					snatch: 117,
+					cleanAndJerk: 142,
+					backSquat: 181,
+					frontSquat: 155
+				},
+				{
+					bodyweight: 94,
+					snatch: 120,
+					cleanAndJerk: 147,
+					backSquat: 187,
+					frontSquat: 161
+				},
+				{
+					bodyweight: 105,
+					snatch: 124,
+					cleanAndJerk: 152,
+					backSquat: 194,
+					frontSquat: 166
+				},
+				{
+					bodyweight: -105,
+					snatch: 135,
+					cleanAndJerk: 165,
+					backSquat: 210,
+					frontSquat: 180
+				}
+			],
+			women: [
+				{
+					bodyweight: 48,
+					snatch: 51,
+					cleanAndJerk: 63,
+					backSquat: 80,
+					frontSquat: 68
+				},
+				{
+					bodyweight: 53,
+					snatch: 56,
+					cleanAndJerk: 68,
+					backSquat: 87,
+					frontSquat: 75
+				},
+				{
+					bodyweight: 58,
+					snatch: 64,
+					cleanAndJerk: 78,
+					backSquat: 99,
+					frontSquat: 85
+				},
+				{
+					bodyweight: 63,
+					snatch: 68,
+					cleanAndJerk: 83,
+					backSquat: 106,
+					frontSquat: 91
+				},
+				{
+					bodyweight: 69,
+					snatch: 70,
+					cleanAndJerk: 86,
+					backSquat: 109,
+					frontSquat: 93
+				},
+				{
+					bodyweight: 75,
+					snatch: 72,
+					cleanAndJerk: 88,
+					backSquat: 112,
+					frontSquat: 96
+				},
+				{
+					bodyweight: -75,
+					snatch: 78,
+					cleanAndJerk: 96,
+					backSquat: 122,
+					frontSquat: 104
+				}
+			]
+		},
+		{
+			men: [
+				{
+					bodyweight: 56,
+					snatch: 90,
+					cleanAndJerk: 110,
+					backSquat: 140,
+					frontSquat: 120
+				},
+				{
+					bodyweight: 62,
+					snatch: 110,
+					cleanAndJerk: 135,
+					backSquat: 171,
+					frontSquat: 147
+				},
+				{
+					bodyweight: 69,
+					snatch: 125,
+					cleanAndJerk: 153,
+					backSquat: 195,
+					frontSquat: 167
+				},
+				{
+					bodyweight: 77,
+					snatch: 137,
+					cleanAndJerk: 168,
+					backSquat: 213,
+					frontSquat: 183
+				},
+				{
+					bodyweight: 85,
+					snatch: 147,
+					cleanAndJerk: 179,
+					backSquat: 228,
+					frontSquat: 196
+				},
+				{
+					bodyweight: 94,
+					snatch: 154,
+					cleanAndJerk: 189,
+					backSquat: 240,
+					frontSquat: 206
+				},
+				{
+					bodyweight: 105,
+					snatch: 158,
+					cleanAndJerk: 193,
+					backSquat: 245,
+					frontSquat: 210
+				},
+				{
+					bodyweight: -105,
+					snatch: 169,
+					cleanAndJerk: 207,
+					backSquat: 263,
+					frontSquat: 226
+				}
+			],
+			women: [
+				{
+					bodyweight: 48,
+					snatch: 64,
+					cleanAndJerk: 78,
+					backSquat: 100,
+					frontSquat: 86
+				},
+				{
+					bodyweight: 53,
+					snatch: 72,
+					cleanAndJerk: 88,
+					backSquat: 112,
+					frontSquat: 96
+				},
+				{
+					bodyweight: 58,
+					snatch: 83,
+					cleanAndJerk: 101,
+					backSquat: 129,
+					frontSquat: 111
+				},
+				{
+					bodyweight: 63,
+					snatch: 85,
+					cleanAndJerk: 104,
+					backSquat: 133,
+					frontSquat: 114
+				},
+				{
+					bodyweight: 69,
+					snatch: 88,
+					cleanAndJerk: 108,
+					backSquat: 138,
+					frontSquat: 117
+				},
+				{
+					bodyweight: 75,
+					snatch: 95,
+					cleanAndJerk: 116,
+					backSquat: 147,
+					frontSquat: 126
+				},
+				{
+					bodyweight: -75,
+					snatch: 107,
+					cleanAndJerk: 130,
+					backSquat: 166,
+					frontSquat: 142
+				}
+			]
+		},
+		{
+			men: [
+				{
+					bodyweight: 56,
+					snatch: 110,
+					cleanAndJerk: 135,
+					backSquat: 172,
+					frontSquat: 147
+				},
+				{
+					bodyweight: 62,
+					snatch: 127,
+					cleanAndJerk: 155,
+					backSquat: 197,
+					frontSquat: 169
+				},
+				{
+					bodyweight: 69,
+					snatch: 133,
+					cleanAndJerk: 163,
+					backSquat: 207,
+					frontSquat: 178
+				},
+				{
+					bodyweight: 77,
+					snatch: 151,
+					cleanAndJerk: 184,
+					backSquat: 235,
+					frontSquat: 201
+				},
+				{
+					bodyweight: 85,
+					snatch: 160,
+					cleanAndJerk: 196,
+					backSquat: 249,
+					frontSquat: 214
+				},
+				{
+					bodyweight: 94,
+					snatch: 168,
+					cleanAndJerk: 206,
+					backSquat: 262,
+					frontSquat: 224
+				},
+				{
+					bodyweight: 105,
+					snatch: 172,
+					cleanAndJerk: 211,
+					backSquat: 268,
+					frontSquat: 230
+				},
+				{
+					bodyweight: -105,
+					snatch: 186,
+					cleanAndJerk: 228,
+					backSquat: 290,
+					frontSquat: 248
+				}
+			],
+			women: [
+				{
+					bodyweight: 48,
+					snatch: 76,
+					cleanAndJerk: 92,
+					backSquat: 118,
+					frontSquat: 101
+				},
+				{
+					bodyweight: 53,
+					snatch: 85,
+					cleanAndJerk: 103,
+					backSquat: 132,
+					frontSquat: 113
+				},
+				{
+					bodyweight: 58,
+					snatch: 94,
+					cleanAndJerk: 115,
+					backSquat: 146,
+					frontSquat: 125
+				},
+				{
+					bodyweight: 63,
+					snatch: 98,
+					cleanAndJerk: 120,
+					backSquat: 153,
+					frontSquat: 131
+				},
+				{
+					bodyweight: 69,
+					snatch: 103,
+					cleanAndJerk: 126,
+					backSquat: 160,
+					frontSquat: 137
+				},
+				{
+					bodyweight: 75,
+					snatch: 110,
+					cleanAndJerk: 134,
+					backSquat: 171,
+					frontSquat: 146
+				},
+				{
+					bodyweight: -75,
+					snatch: 125,
+					cleanAndJerk: 153,
+					backSquat: 195,
+					frontSquat: 167
+				}
+			]
+		},
+		{
+			men: [
+				{
+					bodyweight: 56,
+					snatch: 131,
+					cleanAndJerk: 160,
+					backSquat: 203,
+					frontSquat: 174
+				},
+				{
+					bodyweight: 62,
+					snatch: 144,
+					cleanAndJerk: 175,
+					backSquat: 223,
+					frontSquat: 191
+				},
+				{
+					bodyweight: 69,
+					snatch: 153,
+					cleanAndJerk: 188,
+					backSquat: 239,
+					frontSquat: 205
+				},
+				{
+					bodyweight: 77,
+					snatch: 164,
+					cleanAndJerk: 201,
+					backSquat: 255,
+					frontSquat: 219
+				},
+				{
+					bodyweight: 85,
+					snatch: 173,
+					cleanAndJerk: 212,
+					backSquat: 270,
+					frontSquat: 231
+				},
+				{
+					bodyweight: 94,
+					snatch: 182,
+					cleanAndJerk: 223,
+					backSquat: 283,
+					frontSquat: 243
+				},
+				{
+					bodyweight: 105,
+					snatch: 188,
+					cleanAndJerk: 230,
+					backSquat: 292,
+					frontSquat: 250
+				},
+				{
+					bodyweight: -105,
+					snatch: 203,
+					cleanAndJerk: 248,
+					backSquat: 316,
+					frontSquat: 271
+				}
+			],
+			women: [
+				{
+					bodyweight: 48,
+					snatch: 86,
+					cleanAndJerk: 106,
+					backSquat: 134,
+					frontSquat: 115
+				},
+				{
+					bodyweight: 53,
+					snatch: 98,
+					cleanAndJerk: 119,
+					backSquat: 152,
+					frontSquat: 130
+				},
+				{
+					bodyweight: 58,
+					snatch: 105,
+					cleanAndJerk: 128,
+					backSquat: 163,
+					frontSquat: 140
+				},
+				{
+					bodyweight: 63,
+					snatch: 111,
+					cleanAndJerk: 135,
+					backSquat: 172,
+					frontSquat: 148
+				},
+				{
+					bodyweight: 69,
+					snatch: 118,
+					cleanAndJerk: 144,
+					backSquat: 183,
+					frontSquat: 157
+				},
+				{
+					bodyweight: 75,
+					snatch: 125,
+					cleanAndJerk: 153,
+					backSquat: 195,
+					frontSquat: 167
+				},
+				{
+					bodyweight: -75,
+					snatch: 144,
+					cleanAndJerk: 176,
+					backSquat: 224,
+					frontSquat: 192
+				}
+			]
+		}
+	];
+
+	var stats = {
+		bodyweight: Number(ref.state.bodyweight),
+		snatch: Number(ref.state.snatch),
+		cleanAndJerk: Number(ref.state.cleanAndJerk),
+		backSquat: Number(ref.state.backSquat),
+		frontSquat: Number(ref.state.frontSquat)
+	}
+
+	stats[label] = value;
+
+	var result = getResults();
+
+	ref.setState(result);
+}
+
+module.exports = setResults;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,230 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import setResults from './Levels';
+import './custom.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+class Contents extends Component{
+	constructor(props) {
+		super(props);
+		this.state = {
+			// config states
+			sex: "men",
+			units: "kg",
+			bodyweight: "",
+			snatch: "",
+			cleanAndJerk: "",
+			backSquat: "",
+			frontSquat: "",
+
+			// calculation states
+			weightclass: "",
+			snatchLevel: "",
+			cleanAndJerkLevel: "",
+			backSquatLevel: "",
+			frontSquatLevel: "",
+			snatchNext: "",
+			cleanAndJerkNext: "",
+			backSquatNext: "",
+			frontSquatNext: ""
+
+		}
+		this.clearForm = this.clearForm.bind(this);
+	}
+
+	setCookie(){
+		var expires = function(d){
+			var year = d.getFullYear()+10;
+			var month = d.getMonth();
+			var day = d.getDay();
+			return new Date(year, month, day).toUTCString();
+		}(new Date());
+		alert(expires);
+	}
+
+	clearForm(e){
+		this.setCookie();
+		this.setState({
+			bodyweight: "",
+			snatch: "",
+			cleanAndJerk: "",
+			backSquat: "",
+			frontSquat: "",
+			weightclass: "",
+		});
+	}
+
+	getLevelUpPercent(ref, lift){
+		var current = Number(ref.state[lift]);
+		var next = Number(ref.state[lift+"Next"]);
+		var percent = current/next;
+		return isFinite(percent) ? percent.toFixed(2) : "";
+	}
+
+	setSex(s){
+		s = (s === "men" || s === "women") ? s : "men";
+		this.setState({sex: s});
+	}
+
+	setUnits(u){
+		u = (u === "kg" || u === "lb") ? u : "kg";
+		this.setState({units: u});
+	}
+
+	renderInput(ref, label){
+		var lift = label.split(" ");
+
+		for (var i = 0; i < lift.length; i++){
+			lift[i] = lift[i][0].toUpperCase()+lift[i].substring(1, lift[i].length).toLowerCase();
+		};
+		lift = lift.join("");
+		lift = lift[0].toLowerCase()+lift.substring(1,lift.length);
+
+		function onChange(e){
+			var state = {};
+			var value = e.target.value;
+			state[lift] = value;
+			ref.setState(state);
+			setResults(ref, lift, e.target.value);
+		}
+
+		function overwriteEnter(e){
+			if (e.keyCode === 13) {
+				e.preventDefault();
+				const form = e.target.form;
+				const index = Array.prototype.indexOf.call(form, e.target);
+				if (index+1 < form.elements.length-1){
+					form.elements[index + 1].focus();
+				} else{
+					form.elements[4].focus();
+				}
+			}
+		}
+
+		return (
+			<div>
+				<label>{label}</label>
+				<input className="form-control" type="number" value={ref.state[lift]} onChange={onChange} onKeyUp={overwriteEnter}/>
+			</div>
+		);
+	}
+
+	render() {
+		return (
+			<div>
+				<h2>Results {this.state.bodyweight ? " - "+this.state.weightclass : ""}</h2>
+				<table className="table table-bordered table-striped table-condensed">
+					<thead>
+						<tr>
+							<th>Lift</th>
+							<th>Level</th>
+							<th>Current</th>
+							<th>Next Level</th>
+							<th>% Level Up</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Snatch</td>
+							<td>{this.state.snatchLevel}</td>
+							<td>{this.state.snatch}</td>
+							<td>{this.state.snatchNext}</td>
+							<td>{this.getLevelUpPercent(this, "snatch")}</td>
+						</tr>
+						<tr>
+							<td>Clean &amp; Jerk</td>
+							<td>{this.state.cleanAndJerkLevel}</td>
+							<td>{this.state.cleanAndJerk}</td>
+							<td>{this.state.cleanAndJerkNext}</td>
+							<td>{this.getLevelUpPercent(this, "cleanAndJerk")}</td>
+						</tr>
+						<tr>
+							<td>Back Squat</td>
+							<td>{this.state.backSquatLevel}</td>
+							<td>{this.state.backSquat}</td>
+							<td>{this.state.backSquatNext}</td>
+							<td>{this.getLevelUpPercent(this, "backSquat")}</td>
+						</tr>
+						<tr>
+							<td>Front Squat</td>
+							<td>{this.state.frontSquatLevel}</td>
+							<td>{this.state.frontSquat}</td>
+							<td>{this.state.frontSquatNext}</td>
+							<td>{this.getLevelUpPercent(this, "frontSquat")}</td>
+						</tr>
+					</tbody>
+				</table>
+				<hr/>
+				<form className="form-group">
+					<label htmlFor="sex">Sex</label>
+					<div>
+						<label className="radio-inline">
+							<input type="radio"name="sex" defaultChecked onClick={()=>this.setSex("men")} />Male</label>
+						<label className="radio-inline"><input type="radio" name="sex" onClick={()=>this.setSex("women")} />Female</label>
+					</div>
+					<label htmlFor="units">Units</label>
+					<div>
+						<label className="radio-inline"><input type="radio" name="units" defaultChecked onClick={()=>this.setUnits("kg")} />Kilograms</label>
+						<label className="radio-inline"><input type="radio" name="units" onClick={()=>this.setUnits("lb")} />Pounds</label>
+					</div>
+					<br/>
+					{this.renderInput(this, "Bodyweight")}
+					{this.renderInput(this, "Snatch")}
+					{this.renderInput(this, "Clean and Jerk")}
+					{this.renderInput(this, "Back Squat")}
+					{this.renderInput(this, "Front Squat")}
+					<button type="button" style={{marginTop:"1em"}} onClick={this.clearForm} className="pull-right btn btn-danger">Clear Form</button>
+				</form>
+				<br/>
+				<br/>
+			</div>
+		);
+	}
+}
+
+class Container extends Component{
+	render() {
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-md-2"></div>
+					<div className="col-md-8">
+						<Contents />
+					</div>
+					<div className="col-md-2"></div>
+				</div>
+			</div>
+		);
+	}
+}
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<header className="navbar navbar-top">
+					<div className="container-fluid">
+						<div className="col-md-1"></div>
+						<div className="col-md-10 text-center">
+							<h1>CATALYST ATHLETICS WEIGHTLIFTING LEVELS CALCULATOR</h1>
+						</div>
+						<div className="col-md-1"></div>
+					</div>
+				</header>
+				<Container />
+				<footer className="navbar navbar-bottom">
+					<div className="container text-center pull-right">
+						<p className="navbar-text">Created by: <a href="https://github.com/TysonNgo/weightlifting-levels">Tyson Ngo</a></p>
+						<p className="navbar-text">Calculations based on <a href="https://www.catalystathletics.com/article/1836/Olympic-Weightlifting-Skill-Levels-Chart/"><em>Olympic Weightlifting Skill Levels Chart</em> by <em>Greg Everett</em></a></p>
+					</div>
+				</footer>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('root')
+);
